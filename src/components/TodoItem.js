c5ba97e2710889f6807a9e684ecb26655cddb8ed
smishra11 <msubhasish11@gmail.com>
2020-08-10 10:37:02 +0530

    changed styling for checkbox

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,18 +4,20 @@ export default class TodoItem extends Component {
   render() {
     const { title, handleDelete, handleEdit, handleComplete } = this.props;
     return (
-      <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
-        <input
-          type="checkbox"
-          className="form-check-input ml-1 checksize"
-          onClick={handleComplete}
-        />
-        <h6 className="mt-1 ml-4">{title}</h6>
+      <li className="list-group-item text-capitalize d-flex justify-content-between align-items-center mb-2">
+        <div className="ml-2">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            onClick={handleComplete}
+          />
+          <h6 className="mt-1 ml-2">{title}</h6>
+        </div>
         <div className="todo-icon">
-          <span className="mx-2 text-success btn" onClick={handleEdit}>
+          <span className="text-success btn" onClick={handleEdit}>
             <i className="fas fa-pen"></i>
           </span>
-          <span className="mx-2 text-danger" onClick={handleDelete}>
+          <span className="ml-1 text-danger btn" onClick={handleDelete}>
             <i className="fas fa-trash"></i>
           </span>
         </div>
